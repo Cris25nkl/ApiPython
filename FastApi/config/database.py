a134ed34dd903d3f8651cccae54831e6ef0544e7
cris25nkl <cristian25florez@gmail.com>
2025-04-08 10:57:25 -0500

    Agregar env

--- FastApi/config/database.py
+++ FastApi/config/database.py
@@ -1,10 +1,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+
+load_dotenv()
 
 # URI de conexión a MongoDB Atlas (Reemplaza con tus credenciales)
-MONGO_URI = "mongodb+srv://cris25nkl:Cristianflorez25@fastapi.uqmm6ru.mongodb.net/?retryWrites=true&w=majority&appName=FastApi"
-
+MONGO_URI = os.getenv("MONGO_URI")
+print(MONGO_URI)
 # Nombre de la base de datos
-DATABASE_NAME = "FastApi"
+DATABASE_NAME = os.getenv("MONGO_NAME")
 
 # Cliente de MongoDB
 client = AsyncIOMotorClient(MONGO_URI)
